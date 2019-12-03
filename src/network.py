f88610faf20de49854fdf4ca8d49c608dbde829a
Tim Van Dyke <vandykti@mail.gvsu.edu>
2019-12-02 19:44:37 -0500

    finished network, putting up slightly revised gameLogic from Grant

--- src/network.py
+++ src/network.py
@@ -1,4 +1,12 @@
+"""
+This is a class to handle all of our connections. Each client will use this to talk to the
+server and it abstracts a lot of the socket logic out of the Host and CentralServer files.
+We followed this tutorial for how to implement our game since our game is similar in how
+the connections would need to be used as it is a turn based game.
+https://techwithtim.net/tutorials/python-online-game-tutorial/online-rock-paper-scissors-p1/
+"""
 import socket
+import pickle
 
 
 class Network:
@@ -7,8 +15,10 @@ class Network:
         self.server = ip  # change to your own IP (maybe make this an arg later)
         self.port = port
         self.addr = (self.server, self.port)
-        self.id = self.connect()
-        print(self.id)
+        self.p = self.connect()
+
+    def getP(self):
+        return self.p
 
     def connect(self):
         try:
@@ -20,11 +30,6 @@ class Network:
     def send(self, data):
         try:
             self.client.send(str.encode(data))
-            return self.client.recv(2048).decode()
+            return pickle.loads(self.client.recv(2048 * 2))
         except socket.error as e:
             print(e)
-
-
-n = Network("192.168.50.55", 5555)
-print(n.send("REEEEEE"))
-print(n.send("REEEEEEEEEEEEE"))
